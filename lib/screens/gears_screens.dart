0ffac46365161d349ee04819e06e1969a3c79302
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-05-04 15:33:03 -0300

    update

--- lib/screens/gears_screens.dart
+++ lib/screens/gears_screens.dart
@@ -8,19 +8,40 @@ class GearsScreen extends StatefulWidget {
 }
 
 class _GearsScreenState extends State<GearsScreen> {
+  String _anim = 'spin1';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          height: 150,
-          width: 150,
-          child: FlareActor(
-            'assets/AnimGears.flr',
-            animation: 'spin1',
-          ),
-        ),
-      ),
+          child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  if (_anim == 'spin1')
+                    _anim = 'spin2';
+                  else
+                    _anim = 'spin1';
+                });
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 200,
+                    width: 200,
+                    child: FlareActor(
+                      'assets/gears.flr',
+                      animation: _anim,
+                      isPaused: false,
+                    ),
+                  ),
+                  Text(
+                    _anim,
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ))),
     );
   }
 }
