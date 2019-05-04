58459edb434525ac1e01361a03e7cada7ed4e46e
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-05-03 21:16:34 -0300

    update

---
+++ lib/screens/gears_screens.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class GearsScreen extends StatefulWidget {
+  GearsScreen({Key key}) : super(key: key);
+
+  _GearsScreenState createState() => _GearsScreenState();
+}
+
+class _GearsScreenState extends State<GearsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          child: FlareActor(
+            'assests/AnimGearsAll.flr',
+            animation: 'spin1',
+          ),
+        ),
+      ),
+    );
+  }
+}
